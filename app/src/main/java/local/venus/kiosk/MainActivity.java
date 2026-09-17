ccd72d8017b66dea80885ed3046c1015723b76f4
dedandy <daniele.debernardi@cimafoundation.org>
2026-09-17 14:42:27 +0200

    fix: [VenusKiosk] respect Android screen timeout

--- app/src/main/java/local/venus/kiosk/MainActivity.java
+++ app/src/main/java/local/venus/kiosk/MainActivity.java
@@ -40,8 +40,7 @@ public class MainActivity extends Activity {
         getWindow().addFlags(
                 WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED
                         | WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD
-                        | WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON
-                        | WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+                        | WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON);
         hideSystemUi();
 
         FrameLayout root = new FrameLayout(this);
